39546d21028a9b1c62b24409f799b207bcf9f6b9
SunshineDevv <sasha_kolesnikov_2017@ukr.net>
2025-07-06 18:09:35 +0300

    Resolve warnings

--- app/src/main/java/com/example/planteria/auth/screens/ErrorBanner.kt
+++ app/src/main/java/com/example/planteria/auth/screens/ErrorBanner.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
@@ -26,7 +25,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.painterResource
